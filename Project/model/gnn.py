acdd869401d4e66744df4ded055078d2c3428903
psuwuxian <sysuwuxian@gmail.com>
2022-04-12 18:41:47 -0400

    modify gnn.py

--- Project/model/gnn.py
+++ Project/model/gnn.py
@@ -6,16 +6,14 @@ import torch.nn as nn
 from random import random
 from torch_geometric.nn import GCNConv, GraphConv
 from torch_geometric.nn import global_max_pool, global_add_pool, global_mean_pool
-
+    
+    
 class Encoder(nn.Module):
     def __init__(self, input_dim, hid_dim):
         super().__init__()
         self.conv1 = GCNConv(input_dim, hid_dim, improved=True)
         self.conv2 = GCNConv(hid_dim, hid_dim * 2, improved=True)
-        self.conv3 = GCNConv(hid_dim * 2, hid_dim * 4, improved=True)
-        self.conv4 = GCNConv(hid_dim * 4, hid_dim * 2, improved=True)
-        self.conv5 = GCNConv(hid_dim * 2, hid_dim, improved=True)
-        self.drop1 = nn.Dropout(0.9)
+        self.conv3 = GCNConv(hid_dim * 2, hid_dim, improved=True)
 
     def forward(self, data, edge_index, batch):
         # batch = [num node * num graphs in batch]
@@ -28,15 +26,10 @@ class Encoder(nn.Module):
         output = self.conv2(output, edge_index)
         output = output.relu()
         output = self.conv3(output, edge_index)
-        output = output.relu()
-        output = self.conv4(output, edge_index)
-        output = output.relu()
-        output = self.conv5(output, edge_index)
-        output = self.drop1(output)
 
         # Readout layer
         # output = [batch size, hid dim]
-        output = global_add_pool(output, batch)
+        output = global_max_pool(output, batch)
         
         return output
 
@@ -49,11 +42,14 @@ class GraphClassifier(nn.Module):
         self.device = device
 
         self.encoder = Encoder(self.input_dim, self.hid_dim)
-        self.fc_out = nn.Linear(self.hid_dim, self.output_dim)
+        self.fc1 = nn.Linear(self.hid_dim, 2 * self.hid_dim)
+        self.fc2 = nn.Linear(2 * self.hid_dim, self.output_dim)
         self.device = device
 
     def forward(self, data):
-        features = self.encoder(data.x, data.edge_index, data.batch).unsqueeze(0)
-        output = self.fc_out(features)
+        features = self.encoder(data.x, data.edge_index, data.batch)
+        output = self.fc1(features)
+        output = output.relu()
+        output = self.fc2(output)
 
         return output
